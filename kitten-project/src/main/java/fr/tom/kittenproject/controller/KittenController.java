e7b0cc5d416d2422346f621e4290e41e2a8d4240
NDook <thomasmludek@gmail.com>
2022-11-23 08:09:45 +0100

    p4 is ok

--- kitten-project/src/main/java/fr/tom/kittenproject/controller/KittenController.java
+++ kitten-project/src/main/java/fr/tom/kittenproject/controller/KittenController.java
@@ -3,10 +3,12 @@ package fr.tom.kittenproject.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.tom.kittenproject.entities.Kitten;
 import fr.tom.kittenproject.repositories.KittenRepository;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 public class KittenController {
 
@@ -43,5 +45,18 @@ public class KittenController {
     public boolean delete(@PathVariable Integer id) {
         kittenRepository.deleteById(id);
         return true;
+    }
+
+    @PutMapping("/kittens/{id}")
+    public Kitten update(@PathVariable Integer id, @RequestBody Kitten kitten) {
+        Kitten kittenToUpdate = kittenRepository.findById(id).get();
+        kittenToUpdate.setName(kitten.getName());
+        kittenToUpdate.setAge(kitten.getAge());
+        kittenToUpdate.setSexe(kitten.getSexe());
+        kittenToUpdate.setRace(kitten.getRace());
+        kittenToUpdate.setDescription(kitten.getDescription());
+        kittenToUpdate.setImage(kitten.getImage());
+        kittenToUpdate.setIsAdopted(kitten.getIsAdopted());
+        return kittenRepository.save(kittenToUpdate);
     }
 }
--- kitten-project/src/main/java/fr/tom/kittenproject/entities/Kitten.java
+++ kitten-project/src/main/java/fr/tom/kittenproject/entities/Kitten.java
@@ -21,30 +21,18 @@ public class Kitten {
 
     private boolean isAdopted;
     
-    /**
-     * @return Integer return the id
-     */
     public Integer getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(Integer id) {
         this.id = id;
     }
 
-    /**
-     * @return String return the name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
     public void setName(String name) {
         this.name = name;
     }
@@ -57,74 +45,43 @@ public class Kitten {
         this.image = image;
     }
 
-    /**
-     * @return String return the race
-     */
     public String getRace() {
         return race;
     }
 
-    /**
-     * @param race the race to set
-     */
     public void setRace(String race) {
         this.race = race;
     }
 
-    /**
-     * @return int return the age
-     */
     public String getAge() {
         return age;
     }
-
-    /**
-     * @param age the age to set
-     */
+    
     public void setAge(String age) {
         this.age = age;
     }
 
-    /**
-     * @return String return the sexe
-     */
     public String getSexe() {
         return sexe;
     }
 
-    /**
-     * @param sexe the sexe to set
-     */
     public void setSexe(String sexe) {
         this.sexe = sexe;
     }
 
-    /**
-     * @return String return the description
-     */
     public String getDescription() {
         return description;
     }
 
-    /**
-     * @param description the description to set
-     */
     public void setDescription(String description) {
         this.description = description;
     }
 
-    /**
-     * @return boolean return the isAdopted
-     */
-    public boolean isAdopted() {
+    public boolean getIsAdopted() {
         return isAdopted;
     }
 
-    /**
-     * @param isAdopted the isAdopted to set
-     */
     public void setIsAdopted(boolean isAdopted) {
         this.isAdopted = isAdopted;
     }
-
 }
